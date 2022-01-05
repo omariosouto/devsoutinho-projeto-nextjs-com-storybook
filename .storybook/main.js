5e66e8a2f28ca19ad697c1ca16470767b48a4daa
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-01-04 22:04:34 -0300

    final do video

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../src/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react"
+}
---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Button({ label }) {
+    return (
+        <button>{label}</button>
+    )
+}
+
+Button.propTypes = {
+    label: PropTypes.string.isRequired,
+};
+
+Button.defaultProps = {
+    // label: 'Valor Padrão',
+};
